698cb927d3b094edbd4b301081efc046a754a856
Silfi <katroshenkartem@mail.com>
2026-01-07 19:19:25 +0200

    Initial commit from new PC

---
+++ backend/app/routes/categories.py
@@ -0,0 +1,23 @@
+from fastapi import status, APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+from ..database import get_db
+from ..services.category_service import CategoryService
+from ..schemas.category import CategoryResponse, CategoryCreate
+
+router = APIRouter(
+    prefix='/api/categories',
+    tags=['Categories']
+)
+
+
+@router.get('', response_model=List[CategoryResponse], status_code=status.HTTP_200_OK)
+def get_categories(db: Session = Depends(get_db)):
+    service = CategoryService(db)
+    return service.get_all_categories()
+
+
+@router.get('/{category_id}', response_model=CategoryResponse, status_code=status.HTTP_200_OK)
+def get_category(category_id: int, db: Session = Depends(get_db)):
+    service = CategoryService(db)
+    return service.get_category_by_id(category_id)
---
+++ backend/app/schemas/product.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Optional
+from .category import CategoryResponse
+
+
+class ProductBase(BaseModel):
+    name: str = Field(..., min_length=3, max_length=200,)
+
+    description: Optional[str] = Field(None, description='Product description')
+    price: float = Field(..., gt=0,
+                         description='Product price(must be positive)')
+
+    category_id: int = Field(..., description='ID of the product category')
+    image_url: Optional[str] = Field(
+        None, description='URL of the product image')
+
+
+class ProductCreate(ProductBase):
+    pass
+
+
+class ProductResponse(BaseModel):
+    id: int = Field(..., description='Unique Product ID')
+    name: str
+    description: Optional[str]
+    price: float
+    category_id: int
+    image_url: Optional[str]
+    created_at: datetime
+    category: CategoryResponse = Field(...,
+                                       description='Category of the product')
+
+    class Config:
+        from_attributes = True
+
+
+class ProductListResponse(BaseModel):
+    products: list[ProductResponse]
+    total: int = Field(..., description='Total number of products')
